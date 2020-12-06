5a5676fb041f727ea8eb3c4d70b2a414e28337d8
KL-Skeleton <877526278@qq.com>
2020-12-06 12:57:00 +0800

    update course

--- utils/cx/into-course.js
+++ utils/cx/into-course.js
@@ -4,11 +4,13 @@ const ocs_config = require('../../ocs.config.js')
 
 function intoCourse(driver, course_url) {
 	return new Promise((resolve, reject) => {
+		//进入页面
 		driver.get(course_url).then(r => {
 			setTimeout(()=>{
+				//获取第一个章节
 				driver.findElements(By.css(ocs_config.cx.into_course.elements.job_a)).then(a=>{
-					console.log(a);
 					 a[0].getAttribute('href').then(href=>{
+						 //进入章节
 					 	driver.get(href).then(r=>{
 					 		resolve(href)
 					 	}).catch(e=>{
